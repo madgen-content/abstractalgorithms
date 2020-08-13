068961f53a382ed69a3122e4859770a47e73fed0
Michael <madgencontent@gmail.com>
2020-08-13 01:01:19 -0400

    PATCH: added nifty comments to reservoir sampling

---
+++ abstractalgorithms/general/reservoir_sampling.py
@@ -0,0 +1,29 @@
+from random import randrange
+
+# select n random items from a large stream of unknown length
+# or from a resource that is too large to fit into memory
+# you need only supply an iterator on that resource
+def reservoir_sample(itr, n):
+    # the buffer of randomly selected 'things'
+    kept = []
+
+    # the first n 'things' get in no matter what
+    for _ in range(n): 
+        kept.append(next(itr))
+    
+    # evaluate the stream until empty
+    while obj := next(itr, None):
+        n += 1
+        rval = randrange(n+1)
+
+        if rval < n:
+            # if the 'thing' makes it in, 
+            # replace one of the buffer items at random
+            replace_index = randrange(n)
+            kept[replace_index] = obj
+    return kept
+
+def example():
+    number_stream = (randrange(101) for _ in range(1000))
+    print(reservoir_sample(itr, 20))
+    return
